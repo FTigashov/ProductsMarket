5642a51ddabf9bc8dd04514617d83f97a545d848
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-12-13 19:09:29 +0300

    Initial commit with creating project

---
+++ src/main/java/com/example/internet_shop_website/InternetShopWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.internet_shop_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetShopWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/internet_shop_website/InternetShopWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internet_shop_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
