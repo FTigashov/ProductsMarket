206f6494aef542ad7c9b82aa1a64e522de39e38a
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-12-29 15:05:36 +0300

    Added spring testing

--- core-service/src/main/java/ru/geekbrains/march/market/core/MarchMarketCoreApplication.java
+++ core-service/src/main/java/ru/geekbrains/march/market/core/MarchMarketCoreApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MarchMarketCoreApplication {
 	// Домашнее задание:
-	// 0. Разобраться с кодом
-	// 1. Перенести все интеграции на WebClient
-	// 2. Переделайте оформление заказа с использования Principal на использование
-	// заголовка username
+	// 1. Разобраться с кодом
+	// 2. Добавьте необходимость указания адреса доставки и номера телефона при оформлении заказа
+	// 3. Регистрация новых пользователей
+	// 4. * Мерж корзин. После того как пользователь залогинился, необходимо на бэке смержить
+	// гостевую и корзину пользователя
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarchMarketCoreApplication.class, args);
